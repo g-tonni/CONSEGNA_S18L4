0de0c1dcaf36a23c4ac48e4290da8ff1d954c913
Giada <tonni.giada.210802@gmail.com>
2026-02-12 14:33:00 +0100

    Initial commit

---
+++ src/main/java/giadatonni/CONSEGNA_S18L4/ConsegnaS18L4Application.java
@@ -0,0 +1,13 @@
+package giadatonni.CONSEGNA_S18L4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsegnaS18L4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsegnaS18L4Application.class, args);
+	}
+
+}
---
+++ src/test/java/giadatonni/CONSEGNA_S18L4/ConsegnaS18L4ApplicationTests.java
@@ -0,0 +1,13 @@
+package giadatonni.CONSEGNA_S18L4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsegnaS18L4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
